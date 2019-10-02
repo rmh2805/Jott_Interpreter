4c54b161a783b64914eb33430548aa56c91588b3
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2019-10-01 20:25:15 -0400

    Fix bug skipping chars

--- src/tokenizer.java
+++ src/tokenizer.java
@@ -33,7 +33,7 @@ public class tokenizer {
                 StringBuilder tok = new StringBuilder();
                 char ch = line.charAt(i);
                 if (Character.isWhitespace(ch)) continue; // Skip whitespace before grabbing a new token
-                if (ch == '/' && ++i < lineLength && line.charAt(i) == '/') break; // Ignore comment
+                if (ch == '/' && i + 1 < lineLength && line.charAt(i + 1) == '/') break; // Ignore comment
                 if (Character.isAlphabetic(ch)) { // Identifier or keyword
                     while (i < lineLength) {
                         ch = line.charAt(i);
